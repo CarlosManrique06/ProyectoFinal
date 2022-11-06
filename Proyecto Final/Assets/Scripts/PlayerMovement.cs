736616b56c8b544b4801a6dd331a9657e10ee61d
CarlosManrique06 <cajumalo06@gmail.com>
2022-11-06 14:46:35 -0500

    PlayerMovement Script Correction
    f

--- Proyecto Final/Assets/Scripts/PlayerMovement.cs
+++ Proyecto Final/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    Animator animator;
+   
+
+    public Camera cam;
+    public float mouseHorizontal = 3.0f;
+    public float mouseVertical = 2.0f;
+
+    public float speed = 0.5f;
+
+    public float minRotation = -65.0f;
+    public float maxRotation = 60.0f;
+    float h_mouse, v_mouse;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cam.transform.localEulerAngles = new Vector3(-v_mouse, 0, 0);
+
+        transform.Rotate(0, h_mouse, 0);
+
+        MovimientoJugador();
+
+        Shooting();
     }
-}
+
+    void MovimientoJugador()
+
+    {
+        float movX = Input.GetAxis("Horizontal");
+
+        float movY = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Strafe", movX);
+
+        animator.SetFloat("Forward", movY);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+
+        transform.Translate(new Vector3(movX, 0, movX) * speed);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -5 * speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 5 * speed, 0);
+        }
+    }
+
+    void Shooting()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("aim", true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("aim", false);
+            animator.SetBool("shoot", true);
+        }
+        else
+        {
+            animator.SetBool("shoot", false);
+        }
+    }
+
+ }
