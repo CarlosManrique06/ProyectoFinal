c774fe09d784a1601b2a2f675463f6cd0a70e704
Alejo <67487581+FernandezAlejo@users.noreply.github.com>
2022-11-06 19:00:00 -0300

    Teleporte

--- Proyecto Final/Assets/Scripts/ArrowShooting.cs
+++ Proyecto Final/Assets/Scripts/ArrowShooting.cs
@@ -7,6 +7,12 @@ public class ArrowShooting : MonoBehaviour
     public float Range = 1000f;
     public GameObject Arrow;
     public GameObject Cam;
+    public GameObject objeto;
+
+    int rndX;
+    int rndY;
+    int rndZ;
+
     void Start()
     {
         
@@ -22,9 +28,21 @@ public class ArrowShooting : MonoBehaviour
     {
         RaycastHit hit;
 
-        Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, Range);
+        if(Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, Range)){ 
 
-        Instantiate(Arrow,hit.point, transform.rotation);
+            Debug.Log(hit.transform.name);
 
+            if(hit.transform.name == "Target") { 
+            rndX = Random.Range(-75, 75);
+            rndY = Random.Range(-0, 50);
+            rndZ = Random.Range(1, 130);
+            objeto.transform.position = new Vector3(rndX, 1, rndZ);
+            objeto.transform.rotation = Quaternion.Euler(0, rndY, 0);
+        } else {
+            Instantiate(Arrow,hit.point, transform.rotation);
+        }  
     }
+
+}
+
 }
