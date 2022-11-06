b8a35605fd80f67d2fdad900a78c24ebaec761a9
Alejo <67487581+FernandezAlejo@users.noreply.github.com>
2022-11-06 19:48:55 -0300

    asdad

--- Proyecto Final/Assets/Scripts/ArrowShooting.cs
+++ Proyecto Final/Assets/Scripts/ArrowShooting.cs
@@ -34,9 +34,9 @@ public class ArrowShooting : MonoBehaviour
 
             if(hit.transform.name == "Target") { 
             rndX = Random.Range(-75, 75);
-            rndY = Random.Range(-0, 50);
+            rndY = Random.Range(5, 50);
             rndZ = Random.Range(1, 130);
-            objeto.transform.position = new Vector3(rndX, 1, rndZ);
+            objeto.transform.position = new Vector3(rndX, rndY, rndZ);
             objeto.transform.rotation = Quaternion.Euler(0, rndY, 0);
         } else {
             Instantiate(Arrow,hit.point, transform.rotation);
