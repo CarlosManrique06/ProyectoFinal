0738728de938b70c20260b61443c851fb417e231
CarlosManrique06 <cajumalo06@gmail.com>
2022-11-07 14:31:33 -0500

    Segunda Parte de Tiro Al ARco
    Nuevo maniquí con texturas y otras cositas

--- Proyecto Final/Assets/Scripts/ArrowShooting.cs
+++ Proyecto Final/Assets/Scripts/ArrowShooting.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ArrowShooting : MonoBehaviour
 {
     public float Range = 1000f;
+    private int Points = 1;
     public GameObject Arrow;
     public GameObject Cam;
     public GameObject objeto;
+    public GameObject RED;
+    public GameObject BLUE;
 
     int rndX;
     int rndY;
     int rndZ;
+    int rndrX;
+    int rndrY;
+    private float rndrZ = 0.5f;
 
     void Start()
     {
@@ -28,21 +34,55 @@ public class ArrowShooting : MonoBehaviour
     {
         RaycastHit hit;
 
-        if(Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, Range)){ 
+        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, Range))
+        {
 
-            Debug.Log(hit.transform.name);
+           
 
-            if(hit.transform.name == "Target") { 
-            rndX = Random.Range(-75, 75);
-            rndY = Random.Range(5, 50);
-            rndZ = Random.Range(1, 130);
-            objeto.transform.position = new Vector3(rndX, rndY, rndZ);
-            objeto.transform.rotation = Quaternion.Euler(0, rndY, 0);
-        } else {
-            Instantiate(Arrow,hit.point, transform.rotation);
-        }  
+            if (hit.transform.name == "Target")
+            {
+                rndX = Random.Range(-75, 75);
+                rndY = Random.Range(5, 50);
+                rndZ = Random.Range(1, 130);
+                objeto.transform.position = new Vector3(rndX, rndY, rndZ);
+                objeto.transform.rotation = Quaternion.Euler(0, rndY, 0);
+                Debug.Log("Points"+ "=" + Points++);
+
+                rndrX = Random.Range(1, 3);
+                rndrY = Random.Range(1, 3);
+                objeto.transform.localScale = new Vector3(rndrX, rndrY, rndrZ);
+            }
+            if (hit.transform.tag == "BLUE")
+            {
+
+                rndX = Random.Range(-75, 75);
+                rndY = Random.Range(1, 65);
+                rndZ = Random.Range(-30, 30);
+                BLUE.transform.position = new Vector3(rndX, rndY, rndZ);
+                Debug.Log("GOTCHA :D");
+            }
+            if (hit.transform.tag == "RED")
+            {
+               
+                rndX = Random.Range(-75, 75);
+                rndY = Random.Range(1, 65);
+                rndZ = Random.Range(-30,30);
+                RED.transform.position = new Vector3(rndX, rndY, rndZ);
+                Debug.Log("You Lose :c");
+            }
+
+           
+            else
+            {
+                Instantiate(Arrow, hit.point, transform.rotation);
+            }
+
+           
+           
+        }
+
+       
+
     }
-
-}
 
 }
